0ea1883eaca79e4d498897144e47634558539832
Daniel Wise <io.dwise@gmail.com>
2024-10-21 05:42:09 -0700

    fix: improve contrast on hero section when in light mode

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ export default async function LandingPage() {
 
   return (
     <main className='flex-1'>
-      <section className='flex w-full items-center justify-center py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-r from-primary to-primary-foreground'>
+      <section className='flex w-full items-center justify-center py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-r from-primary to-primary-foreground dark:from-primary-dark dark:to-primary-foreground-dark'>
         <div className='container px-4 md:px-6'>
           <div className='flex flex-col items-center space-y-4 text-center'>
             <div className='space-y-2'>
-              <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-white'>
+              <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-primary-contrast dark:text-primary-contrast-dark'>
                 Join a Hackathon, Learn, Network, and Create Memories!
               </h1>
-              <p className='mx-auto max-w-[700px] text-white md:text-xl'>
+              <p className='mx-auto max-w-[700px] md:text-xl text-primary-contrast/80 dark:text-primary-contrast-dark/80'>
                 Discover exciting challenges, showcase your skills, and connect
                 with like-minded innovators.
               </p>
